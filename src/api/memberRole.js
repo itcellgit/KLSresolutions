c87e536570d3d6790ba710324aee33dd7826ec4e
nirajvernekar02 <nirajvernekar02@gmail.com>
2025-09-05 12:49:15 +0530

    Added server -ip

--- src/api/memberRole.js
+++ src/api/memberRole.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://31.97.239.8:3000/api';
 
 export const assignRole = async (data, token) => {
   const response = await axios.post(`${API_URL}/members/assignRole`, data, {
